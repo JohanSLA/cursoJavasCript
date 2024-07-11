4175ffafe6cd0a7892bdb388061842ffc912e977
johansla <johans.londonoa@uqvirtual.edu.co>
2024-07-11 16:51:12 -0500

    primer commit del proyecto

---
+++ js/codigoAlertaPrueba.js
@@ -0,0 +1,12 @@
+var nombre = "Johan";
+var apellido= "Londoño";
+var edad = 23;
+var presenteONo = true;
+
+
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log("El tipo de dato de la variable edad es: "+ typeof edad);
+alert ("Bienvenido " + nombre);
