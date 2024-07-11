182a71febe91e3e948ca92f06fd48f7967eda4eb
johansla <johans.londonoa@uqvirtual.edu.co>
2024-07-11 18:20:36 -0500

    creacion de archivo para pruebas

--- js/codigoAlertaPrueba.js
+++ js/codigoAlertaPrueba.js
@@ -10,3 +10,9 @@ console.log(apellido);
 console.log(edad);
 console.log("El tipo de dato de la variable edad es: "+ typeof edad);
 alert ("Bienvenido " + nombre);
+
+if (true) {
+    const segundoNombre="stiven";
+}
+
+console.log(segundoNombre);
