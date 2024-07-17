e73dab55c223bae64165e6ff86f2c51022040709
johansla <johans.londonoa@uqvirtual.edu.co>
2024-07-17 17:31:05 -0500

    Creacion primer ejercicio practica

--- js/codigoAlertaPrueba.js
+++ js/codigoAlertaPrueba.js
@@ -1,18 +1,16 @@
-var nombre = "Johan";
-var apellido= "Londoño";
-var edad = 23;
-var presenteONo = true;
+/**
+ * Aplicacion de practica el cual sirve 
+ * para calcular el valor de total de una cena
+ */
 
 
+var valorComida = 2800 + 1300;
+var valorBebida = 900 + 650;
+var cuentaComensal = valorBebida+valorComida;
 
-console.log(nombre);
-console.log(apellido);
-console.log(edad);
-console.log("El tipo de dato de la variable edad es: "+ typeof edad);
-alert ("Bienvenido " + nombre);
+console.log("El total a pagar es = " + cuentaComensal);
+console.log("El valor de la bebida= " + valorBebida);
+console.log("el valor de la comida es de= " + valorComida);
 
-if (true) {
-    const segundoNombre="stiven";
-}
+alert("El valor de la cena es de: "+cuentaComensal);
 
-console.log(segundoNombre);
